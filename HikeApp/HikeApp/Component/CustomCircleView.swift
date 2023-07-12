46a8dac407e27d7a81dc1c5a88eae98503e76bfd
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-12 10:42:13 -0400

    add custom circle view and update card view

---
+++ HikeApp/HikeApp/Component/CustomCircleView.swift
@@ -0,0 +1,38 @@
+//
+//  CustomCircleView.swift
+//  HikeApp
+//
+//  Created by Thomas Cowern on 7/12/23.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    
+    @State private var isAnimateGradient: Bool = false
+    
+    var body: some View {
+        Circle()
+            .fill(
+                LinearGradient(
+                    colors: [
+                        .customIndigoMedium,
+                        .customSalmonLight
+                    ],
+                    startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                    endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing)
+            )
+            .onAppear {
+                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                    isAnimateGradient.toggle()
+                }
+            }
+            .frame(width: 256, height: 256)
+    }
+}
+
+struct CustomCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCircleView()
+    }
+}
