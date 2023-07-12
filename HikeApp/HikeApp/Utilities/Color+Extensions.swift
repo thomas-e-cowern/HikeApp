87e2b3cd73d93f37234e887859debaf8ab800d73
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-12 09:10:15 -0400

    add utilities folder and color extensions

---
+++ HikeApp/HikeApp/Utilities/Color+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extensions.swift
+//  HikeApp
+//
+//  Created by Thomas Cowern on 7/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("CustomGreenDark")
+    static let customGrayLight = Color("CustomGrayLight")
+    static let customGrayMedium = Color("CustomGrayMedium")
+    static let customIndigoMedium = Color("CustomIndigoMedium")
+    static let customSalmonLight = Color("CustomSalmonLight")
+}
