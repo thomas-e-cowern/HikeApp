a9b430825063ca08420d333cbf04ebeb36b31886
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-12 10:06:01 -0400

    update gradient button style with nil coalescing

--- HikeApp/HikeApp/Utilities/GradientButtonStyle.swift
+++ HikeApp/HikeApp/Utilities/GradientButtonStyle.swift
@@ -16,14 +16,25 @@ struct GradientButton: ButtonStyle {
             .padding(.vertical)
             .padding(.horizontal, 30)
             .background(
+                // conditinal statement with nil coalescing
+                configuration.isPressed ?
                 LinearGradient(
                     colors: [
-                        .customGrayLight,
-                        .customGrayMedium
+                        .customGrayMedium,
+                        .customGrayLight
                     ],
                     startPoint: .top,
                     endPoint: .bottom
                 )
+                :
+                    LinearGradient(
+                        colors: [
+                            .customGrayLight,
+                            .customGrayMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
             )
             .cornerRadius(40)
     }
