eb01f24b058a20bc47414065a5389348d4bb3411
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-12 11:14:58 -0400

    add motion animation viw, update custom circle view.

--- HikeApp/HikeApp/Component/CustomCircleView.swift
+++ HikeApp/HikeApp/Component/CustomCircleView.swift
@@ -12,22 +12,27 @@ struct CustomCircleView: View {
     @State private var isAnimateGradient: Bool = false
     
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(
-                    colors: [
-                        .customIndigoMedium,
-                        .customSalmonLight
-                    ],
-                    startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
-                    endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing)
-            )
-            .onAppear {
-                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
-                    isAnimateGradient.toggle()
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmonLight
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing)
+                )
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimateGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256, height: 256)
+            
+            MotionAnimationView()
+            
+        } //: End of ZStack
+        .frame(width: 256, height: 256)
     }
 }
 
---
+++ HikeApp/HikeApp/Component/MotionAnimationView.swift
@@ -0,0 +1,84 @@
+//
+//  MotionAnimationView.swift
+//  HikeApp
+//
+//  Created by Thomas Cowern on 7/12/23.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: - Properties
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    @State private var isAnimating: Bool = false
+    
+    // MARK: Function
+    // Random coordinate
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    
+    // Random size
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 4...80))
+    }
+    
+    // Random scale
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    
+    // Random speed
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.05...1.0)
+    }
+    
+    // Random delay
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .frame(width: randomSize())
+                    .position(
+                        x: randomCoordinate(),
+                        y: randomCoordinate()
+                    )
+                    .scaleEffect(isAnimating ? randomScale() : 1)
+                    .onAppear(perform: {
+                        withAnimation(
+                            .interpolatingSpring(
+                                stiffness: 0.25,
+                                damping: 0.25
+                            )
+                            .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
+                        ) {
+                            isAnimating = true
+                        }
+                    })
+                
+            }  //: End of ForEach
+        } //: End of ZStack
+        .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup()
+    }
+}
+
+// MARK: - Preview
+struct MotionAnimationViiew_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.teal.ignoresSafeArea()
+            MotionAnimationView()
+        }
+    }
+}
