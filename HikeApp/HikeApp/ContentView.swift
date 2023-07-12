734c0da06961cc5f9ec06fd5ad3fd808c5f99413
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-12 08:45:51 -0400

    add card view to project

--- HikeApp/HikeApp/ContentView.swift
+++ HikeApp/HikeApp/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("image-1")
-            .resizable()
-            .scaledToFit()
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            Color("ColorIndigoMedium"),
+                            Color("ColorSalmonLight")
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing)
+                )
+                .frame(width: 256, height: 256)
+            
+            Image("image-1")
+                .resizable()
+                .scaledToFit()
+        }
     }
 }
 
